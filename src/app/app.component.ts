cc024a423868de1212fa8f5d737dfd4daed71daa
Oscar Serrano <oscarsy.ing.systems@hotmail.com>
2024-08-01 21:16:35 -0600

    SE agrega mapa y se coloca boton de animacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { TituloComponent } from '../components/titulo/titulo.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, TituloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
