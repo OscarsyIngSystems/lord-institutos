c045155ef8025d0e51ae006028ab06254d1b0c7a
Oscar Serrano <oscarsy.ing.systems@hotmail.com>
2025-11-08 15:06:02 -0600

    Se agrega funcion de scroll para regresar a la posicion

--- src/components/directorio/directorio.component.ts
+++ src/components/directorio/directorio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { registros } from '../../assets/directorio.json';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
@@ -9,15 +9,56 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './directorio.component.html',
   styleUrl: './directorio.component.scss',
 })
-export class DirectorioComponent {
+export class DirectorioComponent implements OnInit {
   institutos;
+
+  scroll: any;
+  position: any;
   constructor() {
     this.institutos = registros;
-    window.scrollTo(0, 0);
+
+  }
+
+
+  ngOnInit(): void {
+
+    this.restoreScrollPosition();
+  }
+
+  restoreScrollPosition() {
+    const posStr = localStorage.getItem('scrollPos');
+
+    if (posStr) {
+      this.position = JSON.parse(posStr);
+      console.log(this.position, 'POSITION');
+
+      // Usar setTimeout para asegurar que el DOM esté listo
+      setTimeout(() => {
+        window.scrollTo({
+          top: this.position.y,
+          left: this.position.x,
+          behavior: 'auto' // 'smooth' para animación suave
+        });
+        console.log('Scroll position restored');
+      }, 100);
+    }
+
+    localStorage.removeItem('scrollPos');
   }
 
   verDetalle(institutoSeleccionado: any) {
+    this.scroll = this.getScrollPosition();
+
+    localStorage.setItem('scrollPos', JSON.stringify(this.scroll));
     console.log(institutoSeleccionado, 'Seleciona instituto');
     localStorage.setItem('instSelect', JSON.stringify(institutoSeleccionado));
+  }
+
+
+  getScrollPosition() {
+    return {
+      x: window.pageXOffset || document.documentElement.scrollLeft,
+      y: window.pageYOffset || document.documentElement.scrollTop
+    };
   }
 }
